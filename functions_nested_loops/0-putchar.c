5f4f74e7977cae0c70846b29cb7b79adf68cd54d
ruzu <zrubio4898@gmail.com>
2024-06-19 13:02:38 -0400

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,7 +7,15 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putcher("\n);
 	return (0);
 }
 
