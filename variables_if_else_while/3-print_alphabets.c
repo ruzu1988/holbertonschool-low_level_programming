5ea29373487a622a17788bee23b32b633eb1272e
ruzu <zrubio4898@gmail.com>
2024-06-19 09:15:35 -0400

    c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char ch;
+	int i = 0;
+	char c = 'a';
+
+	while (i< 26)
+	{
 
     for (ch = 'a'; ch <= 'z'; ch++)
     {
