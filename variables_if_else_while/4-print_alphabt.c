d7b403af731ef27524e576f2fcffe593733484ff
ruzu <zrubio4898@gmail.com>
2024-06-19 09:46:03 -0400

    c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	char c = 'a';
+
+	while (i < 26)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+			i++, c++;
+		}
+		else
+		{
+			i++, c++;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
