f290a182e355fa3426a8f2a45d7abb45a10740e7
ruzu <zrubio4898@gmail.com>
2024-06-19 22:42:52 -0400

    alphabet x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,14 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	while (i <= 9
+	while (i <= 9)
 	{
-	for (c = 'a'; c ,= 'z'; c++
-			
+	for (c = 'a'; c <= 'z'; c++)
 	}
 
 	_putchar(c);
-	_putchar('\n')
-	i++
-}
+	_putchar('\n');
+
+	i++;
 	return (0);
+}
