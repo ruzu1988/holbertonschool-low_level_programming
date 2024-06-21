c3ab11d1c364c37cf6d16b3afcb393a235699322
ruzu <zrubio4898@gmail.com>
2024-06-20 22:37:07 -0400

    numbers speak

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers range 0 - 9
+ * 0 to 9
+ *
+ * Return: returns nothing
+ */
+
+void print_numbers(void)
+{
+	int num = 48;
+
+	for (num = 48; num <= 57; num++)
+		{
+			_putchar(num);
+		}
+		_putchar('\n');
+}
