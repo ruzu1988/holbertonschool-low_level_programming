3d1697af76fb54c0a05dcb3fafd0db278a12c129
ruzu <zrubio4898@gmail.com>
2024-06-20 22:51:47 -0400

    print most number

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - except 2 and 4
+ *
+ * Return: return void
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 && num != 4)
+		{
+			_putchar(num + '0');
+		}
+	}
+	_putchar('\n');
+}
