1cdc68621886e3e9de9f073336948002b07cceb3
ruzu <zrubio4898@gmail.com>
2024-06-24 23:20:21 -0400

    rev string

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,32 +1,26 @@
 #include "main.h"
 
 /**
-* print_rev - prints a string in reverse
-* @s: string to print
-*/
-
-void print_rev(char *s)
-
-{
-int len = 0, index = 0;
-len = _strlen(s);
-
-for (index = len - 1; index >= 0; index--)
-	putchar(s[index]);
-putchar('\n');
-}
-
-/**
- *_strlen - returns the length of a string
- * @s: string
- *Return: returns lenght;
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
  */
-int _strlen(char *s)
+void print_rev(char *s)
 {
-int count, inc;
-inc = 0;
-for (count = 0; s[count] != '\0'; count++)
-inc++;
+	int i, j, len;
 
-return (inc);
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
 }
