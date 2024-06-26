db603f74d3cf2f1d4fc896aba3285cbfe74388af
ruzu <zrubio4898@gmail.com>
2024-06-26 06:05:02 -0400

    more numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,21 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 0 to 14 10 times
- *
- * Return: return void
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * Return: (void)
  */
-
-void more_numbers(void);
+void more_numbers(void)
 {
-	int i, j;
-	
-	for (i = 0; i <= 9; i++)
+	int num, rep;
+
+	for (rep = 0; rep < 10; rep++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (num = 0; num < 15; num++)
 		{
-			if (j >= 10)
-			{
-				_putchar((j / 10) + '0');
-			}
-			_putchar((j % 10) + '0');
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
 		}
-		_putchar(10);
+		_putchar('\n');
 	}
 }
