d7520e443d85e1289d6a4ae1d9919d044d565496
ruzu <zrubio4898@gmail.com>
2024-06-26 06:08:45 -0400

    diagonals

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,27 +1,30 @@
 #include "main.h"
 
 /**
-* print_diagonal - prints a diagonal
-* @n:parameter
-* Return:returns nothing
-*/
-
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: lenght of diagonal
+ * Return: (int)
+ */
 void print_diagonal(int n)
 {
-	int len, space;
-	
+	int space;
+	int line = 0;
+
 	if (n > 0)
 	{
-		for (len = 0; len < n; len++)
+		for (; n != 0; n--)
 		{
-			for (space = 0; space < len; space++)
-				_putchar(' ');
-			_putchar('\\');
-			
-			if (len == (n - 1))
-				continue;
+			_putchar(92);
 			_putchar('\n');
+			line++;
+			if (n > 1)
+			{
+				space = line;
+				for (; space > 0; space--)
+					_putchar(' ');
+			}
 		}
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
