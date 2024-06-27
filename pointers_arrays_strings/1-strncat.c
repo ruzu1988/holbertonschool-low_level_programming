c0c2032a64ed4ae3f2fadc9daf244f1cb9accc31
ruzu <zrubio4898@gmail.com>
2024-06-26 21:43:07 -0400

    1strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two strings up to certain limit
+ * @n: given limit
+ * @src: source string to concatenate
+ * @dest: target string
+ * Return: resulting dest string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int size;
+	int limit;
+	int path = 0;
+
+	for (size = 0; dest[size] != '\0'; size++)
+
+	for (limit = 0; src[limit] != '\0' && path < n; limit++, path++)
+	{
+		dest[size] = src[limit];
+		size++;
+	}
+	dest[size++] = '\0';
+	return (dest);
+}
