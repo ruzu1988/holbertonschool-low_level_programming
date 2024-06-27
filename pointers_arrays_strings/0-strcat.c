9a7bc5b15f8abbc09447f120cdd954886a932074
ruzu <zrubio4898@gmail.com>
2024-06-26 21:54:42 -0400

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -17,9 +17,9 @@ char *_strcat(char *dest, char *src)
 
 	for (j = 0; src[j] != '\0'; j++)
 	{
-		dest[i+j] = src[j];
+		dest[i + j] = src[j];
 	}
-	dest[i+j] = '\0';
+	dest[i + j] = '\0';
 
 	return (dest);
 }
