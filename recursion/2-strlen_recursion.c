d7413b4d27dcb8f89491d48a7d206e76fd50133f
ruzu <zrubio4898@gmail.com>
2024-07-01 15:02:18 -0400

    strlen recursion

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * int_strlen_recursion - function that returns the length os a string.
- * @s - string
- * Return:
+ * _strlen_recursion - returns length of string
+ * @s: string
+ * Return: string length
  */
 
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return(0);
+		return (0);
 	}
 
 	return (1 + _strlen_recursion(s + 1));
