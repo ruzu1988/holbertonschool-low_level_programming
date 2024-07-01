66c0a25dadf3cd7b3aed925053f65252d908b6e8
ruzu <zrubio4898@gmail.com>
2024-07-01 13:58:39 -0400

    puts recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -10,11 +10,12 @@
 void _puts_recursion(char *s)
 {
 	if (*s <= '\0')
-		_putchar('\n');
+	{
+		putchar('\n');
+		return;
+	}
 
 	_putchar(*s);
-	s++
-		_puts_recursion(s);
-
-	return (0);
+	s++;
+	_puts_recursion(s);
 }
