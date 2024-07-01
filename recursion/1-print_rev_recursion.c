439267f112462504938eca2bcb8670057c5bb7e9
ruzu <zrubio4898@gmail.com>
2024-07-01 14:23:19 -0400

    print rev recursion

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,2 +1,18 @@
 #include "main.h"
-#include <stdio.h>
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse.
+ * @s: string
+ * Return:
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s);
+	_putchar(*s);
+	s++;
+}
