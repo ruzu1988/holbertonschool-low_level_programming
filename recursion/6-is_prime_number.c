ad6afbaa52cfb9610252e71f7c45f84afd97ca88
ruzu <zrubio4898@gmail.com>
2024-07-01 23:59:25 -0400

    prime

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,27 +1,29 @@
 #include "main.h"
 
 /**
- * is_prime_number - func that returns 1 if a num is a prime num, 0 otherwise
- * @n: number for input
- * Return: 0
+ * is_prime_number - checks if a number is a prime number
+ * @n: number to check
+ *
+ * Return: 1 if n is a prime, 0 otherwise
  */
 
 int is_prime_number(int n)
-	return (is_prime_number(n, 2, n / 2));
+{
+	return (helper_prime(n, 2, n / 2));
+}
 
 /**
- *  helper_prime - blank
- * @n: checkl
- * @i: for the increment
+ * helper_prime - check if a number is prime
+ * @n: the number to check
+ * @i: for current divisor being tested
  * @limit: stop
- * Return: 0
+ * Return: 1 if n is prime, 0 otherwise
  */
 
 int helper_prime(int n, int i, int limit)
 
 	if ((n % i == 0 && i <= limit) || n < 0 || n == 1)
-		return (0);
-	else if (n % i != 0 && i <= limit)
-		return (helper_prime(n, i + 1, limit));
-	else
-		return (1);
+	return (0);
+	if (i > limit)
+	return (1);
+	return (helper_prime(n, i + 1, limit));
