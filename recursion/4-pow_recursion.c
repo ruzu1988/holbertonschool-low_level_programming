4fec67603bd0e0d3cb18aa9b0f707a7f20635d3e
ruzu <zrubio4898@gmail.com>
2024-07-01 22:05:29 -0400

    pow

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _pow_recursion - returns the value of x to the power of y
+ * _pow_recursion - returns the value of x raised to the power of y
  * @x: base number
  * @y: exponential
  * Return: result
@@ -9,7 +10,7 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y <= 0)
+	if (y < 0)
 		return (-1);
 	if (y == 0)
 		return (1);
