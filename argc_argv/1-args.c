7e5ec48904624add7ea06697c7f3e02a786f1139
ruzu <zrubio4898@gmail.com>
2024-07-02 20:22:28 -0400

    args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - value
+ * @argc: value
+ * @argv: varibale
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; argv == 0; i++)
+	{
+		printf("%s\n", argc[argv]);
+	}
+	return (0);
+}
