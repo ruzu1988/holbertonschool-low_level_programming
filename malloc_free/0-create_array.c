f147dbcd8c648f2549f981fb44376609df778b52
ruzu <zrubio4898@gmail.com>
2024-07-03 14:50:20 -0400

    create array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -5,24 +5,30 @@
  * create_array - create array char
  * @z: pointer array
  * @size: memory to print
+ * @c: char
  * Return: array
  */
 
 char *create_array(unsigned int size, char c)
 {
 	int i;
+
 	char *z;
-	
+
 	if (size == 0)
+	{
 		return (NULL);
-	
+	}
+
 	z = malloc(sizeof(char) * size);
-	
 	if (z == NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; z[i] == '\0'; i++)
+	{
 		z[i] = c;
-	
+	}
 	return (z);
 }
