d6565e81ba6e5e3d6c5cce03d05544021c4d8235
ruzu <zrubio4898@gmail.com>
2024-07-03 23:41:22 -0400

    duplicated

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - funtion returns a pointer to a new string
+ * @str: the souce string
+ * free:
+ * Return: NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *dupli;
+	int longnom;
+
+	if (dupli == NULL)
+		return (NULL);
+
+		dupli = malloc((longnom)) * sizeof(char))
+		dupli = *str;
+
+	if (str == NULL)
+		return (NULL);
+
+		free(dupli);
+
+	return (0);
+}
+
