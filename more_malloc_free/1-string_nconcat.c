6bb577b6f09e453e6bb4694d83200010b2d4591c
ruzu <zrubio4898@gmail.com>
2024-07-05 00:04:43 -0400

    string

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /**
- * string_nconcat - concatenate two strings using n amount of s2
+ * string_nconcat - function that cocatenates two strings
  * @s1: first string
  * @s2: string to add to end of s1
  * @n: amount of s2 to add to s1
@@ -17,29 +17,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
-	if (s2 ==NULL)
+	if (s2 == NULL)
 		s2 = "";
 
 	len_s1 = srtlen(s1);
 	len_s2 = strlen(s2);
 
 	if (n >= len_s2)
-	{
 		n = len_s2;
-		concat_str = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
-	}
-	
+
 	len_concat = len_s1 + n;
 
 	concat_str = malloc(len_concat + 1);
 
-	if(concat_str == NULL)
-		return NULL;
+	if (concat_str == NULL)
+		return (NULL);
+
+	for (i = 0; i < len_s1; i++)
+		concat_str[i] = s1[j];
 
 	for (j = 0; j < n; j++)
-		concat_str[i] = s2[j];
-
 	concat_str[i + j] = s2[j];
+
+	concat_str[i + j] = '\0';
 
 	return (concat_str);
 }
