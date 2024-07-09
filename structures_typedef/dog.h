8a33c4900de986a7f487947a5ed0dab028782fd1
ruzu <zrubio4898@gmail.com>
2024-07-09 11:42:39 -0400

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,10 +8,10 @@
  */
 typedef struct dog
 {
-  char *name;
-  float age;
-  char *owner;
-}dog_t;
+char *name;
+float age;
+char *owner;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
