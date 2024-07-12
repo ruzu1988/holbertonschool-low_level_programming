5d626cb6ac231c2191ecba8d99e1098a83d00e9d
ruzu <zrubio4898@gmail.com>
2024-07-12 14:34:14 -0400

    hola

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
 #include "function_pointers.h"
+
+/**
+ * print_name - print name
+ * @name: name of the person
+ * @f: fuction
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
+
