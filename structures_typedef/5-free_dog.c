717893dc8fe3f2544c24846cdd679afbf63b214f
ruzu <zrubio4898@gmail.com>
2024-07-12 13:09:17 -0400

    fredog

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
-#inclede <stdlib>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
  * free_dog - free spaces
- * @name: free dog
- * @owner: free
  *
- * Return: 0
+ * @d: struct
  */
 void free_dog(dog_t *d)
 {
 	if (d != NULL)
 		return;
-	
-	if (d -> name)
+	if (d->name)
 		free(d->name);
-
-	if (d -> owner)
+	if (d->owner)
 		free(d->owner);
 
 		free(d);
