1c378eb0361cc24c2fb33fbdad2d6d93b86aa2c7
ruzu <zrubio4898@gmail.com>
2024-07-12 13:07:58 -0400

    newdog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -21,14 +21,13 @@ char *_strdup(char *s)
 
 	str = malloc(sizeof(char) * (i + 1));
 	if (str == NULL)
-		return( NULL);
+		return (NULL);
 
-	for ( j = 0; s[j] != '\0'; j++)
+	for (j = 0; s[j] != '\0'; j++)
 	{
 		str[j] = s[j];
 	}
 	str[j] = '\0';
-		
 	return (str);
 }
 
@@ -42,22 +41,21 @@ char *_strdup(char *s)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	
+
 	if (name == NULL || owner == NULL)
 		return (NULL);
-	
 	new_dog = malloc(sizeof(dog_t));
+
 	if (new_dog == NULL)
 		return (NULL);
-	
 	new_dog->name = _strdup(name);
 	if (new_dog->name  == NULL)
 	{
-		free (new_dog);
+		free(new_dog);
 		return (NULL);
 	}
 
-	new_dog->owner = _strdup (owner);
+	new_dog->owner = _strdup(owner);
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
@@ -66,7 +64,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 	new_dog->age = age;
-	
 	return (new_dog);
 }
 
