1c8b7eba4e3c559ebb2f156bafe9e8a405513a2a
ruzu <zrubio4898@gmail.com>
2024-07-14 18:01:38 -0400

    int index

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+
+/*
+ * int_index - returns the index of the first element for wich
+ * @cmp: is a pointer to the function to be used to compare values
+ * @size: is the number
+ * @array: elemnts in the array
+ * Return: -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int indpal;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+
+	for (indpal = 0; indpal < size; indpal++)
+	{
+		if (cmp(array[indpal]))
+			return (indpal);
+	}
+	return (-1);
+}
