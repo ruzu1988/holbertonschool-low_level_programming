e4cbd22408dfbf00cb986da3f68bd24e5896f24c
ruzu <zrubio4898@gmail.com>
2024-07-14 21:05:08 -0400

    sum them all

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - suma todos los argumentos pasados a la funcion
+ * @n: numero de argumentos
+ *
+ * Return: 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list argumentos;
+	unsigned int ind;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(argumentos, n);
+
+	for (ind = 0; ind < n; ind++)
+		sum += va_arg(argumentos, int);
+
+	va_end(argumentos);
+
+	return (sum);
+}
