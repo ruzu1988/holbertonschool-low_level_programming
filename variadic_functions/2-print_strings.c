4bbaa26432a061435302ee9fa7284391f7be99ef
ruzu <zrubio4898@gmail.com>
2024-07-15 09:10:19 -0400

    print string

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -3,7 +3,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_string - prints strings, followed by separator a new line
+ * print_strings - prints strings, followed by separator and a new line
  * @n: numbers of strings
  * @separator: string printer
  */
@@ -12,20 +12,22 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list argumentos;
 	unsigned int indpal;
-	char *sepa;
+	char *x;
 
 	va_start(argumentos, n);
 
 	for (indpal = 0; indpal < n; indpal++)
 	{
-		sepa = va_arg(argumentos, char *);
-		if (sepa != NULL)
-			printf("(nil)");
+		x = va_arg(argumentos, char *);
+		if (x != NULL)
+			printf("%s", x);
 		else
-			printf("%s", sepa);
-		if (indpal < n - 1 || separator)
-		printf("%s", separator);
+			printf("(nil)");
+
+		if (separator != NULL && indpal < n - 1)
+			printf("%s", separator);
 	}
+
 	va_end(argumentos);
 	printf("\n");
 }
